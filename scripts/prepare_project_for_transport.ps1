8bd01df915b7dde5fbb372cde311f427e93f07fb
Krindendo <lollipopfunkyx9@gmail.com>
2022-08-21 13:44:38 +0200

    updated ps to delete more files

--- scripts/prepare_project_for_transport.ps1
+++ scripts/prepare_project_for_transport.ps1
@@ -4,11 +4,13 @@ Remove-Item -LiteralPath "./node_modules" -Force -Recurse
 Remove-Item -LiteralPath "./packages/eslint-config-custom/node_modules" -Force -Recurse
 Remove-Item -LiteralPath "./packages/eslint-config-custom-server/node_modules" -Force -Recurse
 Remove-Item -LiteralPath "./packages/jest-presets/node_modules" -Force -Recurse
+Remove-Item -LiteralPath "./packages/logger/.turbo" -Force -Recurse
 Remove-Item -LiteralPath "./packages/logger/dist" -Force -Recurse
 Remove-Item -LiteralPath "./packages/logger/node_modules" -Force -Recurse
 Remove-Item -LiteralPath "./apps/native/node_modules" -Force -Recurse
 Remove-Item -LiteralPath "./apps/server/dist" -Force -Recurse
 Remove-Item -LiteralPath "./apps/server/.turbo" -Force -Recurse
 Remove-Item -LiteralPath "./apps/server/node_modules" -Force -Recurse
+Remove-Item -LiteralPath "./apps/web/.next" -Force -Recurse
 Remove-Item -LiteralPath "./apps/web/.turbo" -Force -Recurse
 Remove-Item -LiteralPath "./apps/web/node_modules" -Force -Recurse
