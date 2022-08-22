8758e861769eec64c4bb1a8b47b0baad64b30422
Krindendo <lollipopfunkyx9@gmail.com>
2022-08-23 00:23:37 +0200

    ci: updated powershell script

--- scripts/prepare_project_for_transport.ps1
+++ scripts/prepare_project_for_transport.ps1
@@ -2,13 +2,20 @@
 
 Remove-Item -LiteralPath "./node_modules" -Force -Recurse
 Remove-Item -LiteralPath "./packages/config/node_modules" -Force -Recurse
+
 Remove-Item -LiteralPath "./packages/logger/.turbo" -Force -Recurse
 Remove-Item -LiteralPath "./packages/logger/dist" -Force -Recurse
+Remove-Item -LiteralPath "./packages/logger/coverage" -Force -Recurse
 Remove-Item -LiteralPath "./packages/logger/node_modules" -Force -Recurse
+
 Remove-Item -LiteralPath "./apps/native/node_modules" -Force -Recurse
+
 Remove-Item -LiteralPath "./apps/server/dist" -Force -Recurse
 Remove-Item -LiteralPath "./apps/server/.turbo" -Force -Recurse
+Remove-Item -LiteralPath "./apps/server/coverage" -Force -Recurse
 Remove-Item -LiteralPath "./apps/server/node_modules" -Force -Recurse
+
 Remove-Item -LiteralPath "./apps/web/.next" -Force -Recurse
 Remove-Item -LiteralPath "./apps/web/.turbo" -Force -Recurse
+Remove-Item -LiteralPath "./apps/web/coverage" -Force -Recurse
 Remove-Item -LiteralPath "./apps/web/node_modules" -Force -Recurse
